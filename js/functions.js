5085ed2d52a88401a1063b31b2b8911ae4e5290c
jefferson-calmon <62778628+jefferson-calmon@users.noreply.github.com>
2020-07-12 11:42:37 -0300

    style: Modifield the footer in landing page

--- js/functions.js
+++ js/functions.js
@@ -5,7 +5,28 @@ function toggle(){
     nav.classList.toggle('active')
 }
 
-// function efectMoveLi(){
-//     var li = document.getElementById('items-menu');
-//     li.style.color = 'red'
-// }
+function instagramEffect(id, state){
+    const icon = document.getElementById(id)
+
+    if(state === 'enter'){
+        icon.style.backgroundSize = 'contain'
+        icon.style.background = 'linear-gradient(60deg, #F8D100, #F40106, #BC3389, #8941BD)';
+    }
+    if(state === 'out'){
+        icon.style.background = 'transparent';
+    }
+}
+
+function wpEffect(id, state){
+    const wp = document.getElementById(id);
+
+    if(state === 'enter'){
+        wp.style.backgroundSize = 'contain'
+        wp.style.background = '#3DC24F';
+        wp.style.borderRadius = '20px 20px 20px 17px'
+        wp.style.height = '29px'
+    }
+    if(state === 'out'){
+        wp.style.background = 'transparent';
+    }
+}
